4d2f8a75232a3aa0b31d277bdfe6a3442e6e3418
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-01-09 18:39:27 +0200

    Shop page, collection preview component, collection item component => all ready

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './shop.styles.scss';
+
+import SHOP_DATA from './shop.data';
+
+import CollectionPreview from '../../components/preview-collection/collection-preview.component';
+
+class Shop extends React.Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {
+			collections: SHOP_DATA
+		}
+	}
+
+	render () {
+		return (
+			<div className="shop-page">
+				{
+					this.state.collections.map(({ id, ...otherCollectionProps }) => (
+						<CollectionPreview key={ id } { ...otherCollectionProps } />
+					))
+				}
+			</div>
+		)
+	}
+}
+
+export default Shop;
