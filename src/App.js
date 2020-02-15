282f6667051eea68b77e068fd6708511ffa1f172
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-02-15 18:54:07 +0200

    Sign In (both Email and Google) sagas refactored

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 import Header from './components/header/header.component';
@@ -11,7 +10,6 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInPage from './pages/sign-in/sing-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -19,37 +17,6 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount () {
-		const { setCurrentUser } = this.props;
-
-		/*
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-				// checking if userAuth object is not 'null' (exists)
-				// meaning if a user has signed in
-				if (userAuth) {
-					// createUserProfileDocument returns userRef
-					const userRef = await createUserProfileDocument(userAuth);
-
-					// listening to the moment database has updated at that reference with any new data
-					// 'snapshot' represents data that is currently (in its current state)
-					// stored in our database
-					userRef.onSnapshot(snapShot => {
-						setCurrentUser({
-							id: snapShot.id,
-							...snapShot.data()
-						})
-					});
-				}
-				// if user signs out
-				else {
-					// setting current user to 'null'
-					setCurrentUser(userAuth);
-				}
-			},
-			error => {
-				console.log(error)
-			}
-		);
-		*/
 
 	}
 
@@ -80,11 +47,6 @@ const mapStateToProps = createStructuredSelector({
 	currentUser: selectCurrentUser
 });
 
-const mapDispatchToProps = dispatch => ({
-	setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+	mapStateToProps
 )(App);
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -3,37 +3,41 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 import UserActionTypes from './user.types';
 
 import {
-	googleSignInSuccess,
-	googleSignInFailure,
-	emailSignInSuccess,
-	emailSignInFailure
+	signInSuccess,
+	signInFailure
 } from './user.actions';
 
 import { auth, googleProvider, createUserProfileDocument } from '../../firebase/firebase.utils';
 
-export function* signInWithGoogle () {
+export function* getSnapshotFromUserAuth (user) {
 	try {
-		const { user } = yield auth.signInWithPopup(googleProvider);
 		const userRef = yield call(createUserProfileDocument, user);
 		const userSnapshot = yield userRef.get();
 
-		yield put(googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+		yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
 	}
 	catch (err) {
-		yield put(googleSignInFailure(err));
+		yield put(signInFailure(err));
+	}
+}
+
+export function* signInWithGoogle () {
+	try {
+		const { user } = yield auth.signInWithPopup(googleProvider);
+		yield getSnapshotFromUserAuth(user);
+	}
+	catch (err) {
+		yield put(signInFailure(err));
 	}
 }
 
 export function* signInWithEmail ({ payload: { email, password } }) {
 	try {
 		const { user } = yield auth.signInWithEmailAndPassword(email, password);
-		const userRef = yield call(createUserProfileDocument, user);
-		const userSnapshot = yield userRef.get();
-
-		yield put(emailSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }))
+		yield getSnapshotFromUserAuth(user);
 	}
 	catch (err) {
-		yield put(emailSignInFailure(err));
+		yield put(signInFailure(err));
 	}
 }
 
